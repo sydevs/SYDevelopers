00880aa60d89270d469ce65021a2eb24a1bfb1ce
Devindra <arden.ived@gmail.com>
2021-02-22 14:36:50 -0800

    Update mailer.rb

--- config/mailer.rb
+++ config/mailer.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['SMTP_EMAIL'],
@@ -10,7 +10,6 @@ Rails.application.configure do
     enable_starttls_auto: true,
   }
 
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_options = { from: "SY Developers <#{ENV['SMTP_EMAIL']}>" }
   config.action_mailer.raise_delivery_errors = true
 end
